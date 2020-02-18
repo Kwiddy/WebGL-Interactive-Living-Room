266b5bfb88046cca520fc975a458d296a223022f
Kwiddy <kwiddeson4@gmail.com>
2020-02-18 10:28:04 +0000

    Added chair legs

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var VSHADER_SOURCE =
   'attribute vec4 a_Position;\n' +
   'attribute vec4 a_Color;\n' +
-  'attribute vec4 a_Normal;\n' +        // Normal
+  'attribute vec4 a_Normal;\n' +        
   'uniform mat4 u_ModelMatrix;\n' +
   'uniform mat4 u_NormalMatrix;\n' +
   'uniform mat4 u_ViewMatrix;\n' +
   'uniform mat4 u_ProjMatrix;\n' +
-  'uniform vec3 u_LightColor;\n' +     // Light color
-  'uniform vec3 u_LightDirection;\n' + // Light direction (in the world coordinate, normalized)
+  'uniform vec3 u_LightColor;\n' +     
+  'uniform vec3 u_LightDirection;\n' + 
   'varying vec4 v_Color;\n' +
   'uniform bool u_isLighting;\n' +
   'void main() {\n' +
@@ -16,7 +16,6 @@ var VSHADER_SOURCE =
   '  {\n' +
   '     vec3 normal = normalize((u_NormalMatrix * a_Normal).xyz);\n' +
   '     float nDotL = max(dot(normal, u_LightDirection), 0.0);\n' +
-        // Calculate the color due to diffuse reflection
   '     vec3 diffuse = u_LightColor * a_Color.rgb * nDotL;\n' +
   '     v_Color = vec4(diffuse, a_Color.a);\n' +  '  }\n' +
   '  else\n' +
@@ -106,7 +105,7 @@ function keydown(ev, gl, u_ModelMatrix, u_NormalMatrix, u_isLighting) {
         case 37: //left
             g_yAngle = (g_yAngle - ANGLE_STEP) % 360;
             break;
-        case 39:
+        case 39: //right
             g_yAngle = (g_yAngle + ANGLE_STEP) % 360;
             break;
         default: return;
@@ -272,18 +271,10 @@ function initAxesVertexBuffers(gl) {
   
     modelMatrix.setTranslate(0, 0, 0);  
     modelMatrix.rotate(g_yAngle, 0, 1, 0); 
-    modelMatrix.rotate(g_xAngle, 1, 0, 0); 
-  
-    pushMatrix(modelMatrix);
-    modelMatrix.scale(2.0, 0.5, 2.0); 
-    drawbox(gl, u_ModelMatrix, u_NormalMatrix, n);
-    modelMatrix = popMatrix();
-  
-    pushMatrix(modelMatrix);
-    modelMatrix.translate(0, 1.25, -0.75);  
-    modelMatrix.scale(2.0, 2.0, 0.5);
-    drawbox(gl, u_ModelMatrix, u_NormalMatrix, n);
-    modelMatrix = popMatrix();
+    modelMatrix.rotate(g_xAngle, 1, 0, 0);
+    
+    buildChair(gl, u_ModelMatrix, u_NormalMatrix, u_isLighting, n);
+    
   }
   
   function drawbox(gl, u_ModelMatrix, u_NormalMatrix, n) {
@@ -298,4 +289,42 @@ function initAxesVertexBuffers(gl) {
       gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
   
     modelMatrix = popMatrix();
+  }
+
+  function buildChair(gl, u_ModelMatrix, u_NormalMatrix, u_isLighting, n) {
+    pushMatrix(modelMatrix);
+    modelMatrix.scale(2.0, 0.5, 2.0); 
+    drawbox(gl, u_ModelMatrix, u_NormalMatrix, n);
+    modelMatrix = popMatrix();
+  
+    pushMatrix(modelMatrix);
+    modelMatrix.translate(0, 1.25, -0.75);  
+    modelMatrix.scale(2.0, 2.0, 0.5);
+    drawbox(gl, u_ModelMatrix, u_NormalMatrix, n);
+    modelMatrix = popMatrix();
+
+    pushMatrix(modelMatrix);
+    modelMatrix.translate(0.75, -1, 0.75);  
+    modelMatrix.scale(0.5, 2.0, 0.5);
+    drawbox(gl, u_ModelMatrix, u_NormalMatrix, n);
+    modelMatrix = popMatrix();
+
+    pushMatrix(modelMatrix);
+    modelMatrix.translate(-0.75, -1, 0.75);  
+    modelMatrix.scale(0.5, 2.0, 0.5);
+    drawbox(gl, u_ModelMatrix, u_NormalMatrix, n);
+    modelMatrix = popMatrix();
+
+    pushMatrix(modelMatrix);
+    modelMatrix.translate(0.75, -1, -0.75);  
+    modelMatrix.scale(0.5, 2.0, 0.5);
+    drawbox(gl, u_ModelMatrix, u_NormalMatrix, n);
+    modelMatrix = popMatrix();
+
+    pushMatrix(modelMatrix);
+    modelMatrix.translate(-0.75, -1, -0.75);  
+    modelMatrix.scale(0.5, 2.0, 0.5);
+    drawbox(gl, u_ModelMatrix, u_NormalMatrix, n);
+    modelMatrix = popMatrix();
+
   }
